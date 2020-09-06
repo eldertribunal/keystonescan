c959455adc18ff4ad166081a444359d9c9f69a3d
Drew Frezell <dfrezell@gmail.com>
2020-09-06 12:02:48 -0400

    Generate character and dungeon data as well

--- keystonescan/keystonescan.py
+++ keystonescan/keystonescan.py
@@ -11,6 +11,7 @@ import json
 import copy
 import datetime
 from datetime import timezone
+from datetime import timedelta
 
 from keystonescan.blizzoauth import BlizzOAuth
 from keystonescan.blizzrequest import BlizzardApiRequest
@@ -32,36 +33,34 @@ def get_dungeon_defaults(blizzapi):
     mythic_dungeons = blizzapi.mythic_keystone_dungeon_index()
     for mythic_dungeon in mythic_dungeons["dungeons"]:
         dungeon = blizzapi.mythic_keystone_dungeon(mythic_dungeon["id"])
+        media = blizzapi.media_journal_instance(dungeon["dungeon"]["id"])
+        upgrade_times = [str(timedelta(milliseconds=duration))
+                for _, duration in [keystone_upgrades.values()
+                    for keystone_upgrades in dungeon["keystone_upgrades"]]]
+        upgrade_times.sort(reverse=True)
+        tile = None
+        for asset in media["assets"]:
+            if asset["key"] == "tile":
+                tile = asset["value"]
         dungeon_default.append({
             "name": dungeon["name"],
             "level": 0,
-            "id": dungeon["id"]
+            "id": dungeon["id"],
+            "upgrade": upgrade_times,
+            "tile": tile
         })
+
     return dungeon_default
 
 def scan_completed_keystones(blizzapi, scanned_toons):
     '''
     Get all toons from `<input_dir>/toons.json and query the blizzard API
     for all the completed keystones.
-
-    {
-        "Player": {
-            "Atal Dazzar": 15,
-            "Kings' Rest": 12,
-        }
-    }
     '''
-    players = {}
     for toon in scanned_toons:
         print("scanning {}".format(toon))
         toon.get_mythic_keystone(blizzapi)
-        if toon.player in players:
-            for dungeon, lvl in toon.keystone.items():
-                if dungeon not in players[toon.player] or players[toon.player][dungeon] < lvl:
-                    players[toon.player][dungeon] = lvl
-        else:
-            players[toon.player] = toon.keystone
-    return players
+    return scanned_toons
 
 def get_toons(input_dir):
     '''
@@ -87,6 +86,53 @@ def get_api_access_file(input_dir):
     return (access["client_id"], access["client_secret"])
 
 
+def generate_player_output(output_dir, characters, dungeon_default):
+    '''
+    generate_player_output
+    '''
+    # collapse all characters with the same player into a dictionary
+    player_cache = {}
+    for character in characters:
+        if character.player not in player_cache:
+            player_cache[character.player] = copy.deepcopy(dungeon_default)
+
+        for dungeon in player_cache[character.player]:
+            dname, dlevel, _, _, _ = dungeon.values()
+            if dname in character.keystone and dlevel < character.keystone[dname]:
+                dungeon["level"] = character.keystone[dname]
+
+    with open(os.path.join(output_dir, "players.json"), mode="w") as players_fd:
+        json.dump(
+            [{"name": player, "dungeons": dungeons} for player, dungeons in player_cache.items()],
+            players_fd, indent=2)
+
+def generate_character_output(output_dir, characters, dungeon_default):
+    '''
+    generate_character_output
+    '''
+    character_cache = {}
+    for character in characters:
+        character_cache[character.name] = copy.deepcopy(dungeon_default)
+
+        for dungeon in character_cache[character.name]:
+            dname, dlevel, _, _, _ = dungeon.values()
+            if dname in character.keystone and dlevel < character.keystone[dname]:
+                dungeon["level"] = character.keystone[dname]
+
+    with open(os.path.join(output_dir, "characters.json"), mode="w") as character_fd:
+        json.dump(
+            [{"name": str.capitalize(character), "dungeons": dungeons}
+                    for character, dungeons in character_cache.items()],
+            character_fd, indent=2)
+
+def generate_dungeon_output(output_dir, dungeon_default):
+    '''
+    generate_dungeon_output
+    '''
+    with open(os.path.join(output_dir, "dungeons.json"), mode="w") as dungeon_fd:
+        json.dump(dungeon_default, dungeon_fd, indent=2)
+
+
 def scan(input_dir, output_dir):
     '''
     Scan all the characters and write the formatted data to <output_dir>
@@ -97,21 +143,11 @@ def scan(input_dir, output_dir):
             os.path.join(input_dir, "auth_cache.json")))
 
     dungeon_default = get_dungeon_defaults(blizzapi)
-    players = scan_completed_keystones(blizzapi, get_toons(input_dir))
+    characters = scan_completed_keystones(blizzapi, get_toons(input_dir))
 
-    norm_players = []
-    for player, dungeons in players.items():
-        norm_player = {
-            "name": player,
-            "dungeons": copy.deepcopy(dungeon_default),
-        }
-        for norm_dungeon in norm_player["dungeons"]:
-            if norm_dungeon["name"] in dungeons:
-                norm_dungeon["level"] = dungeons[norm_dungeon["name"]]
-        norm_players.append(norm_player)
-
-    with open(os.path.join(output_dir, "players.json"), mode="w") as players_fd:
-        json.dump(norm_players, players_fd, indent=2)
+    generate_player_output(output_dir, characters, dungeon_default)
+    generate_character_output(output_dir, characters, dungeon_default)
+    generate_dungeon_output(output_dir, dungeon_default)
 
     with open(os.path.join(output_dir, "scanned.json"), mode="w") as scanned_fd:
         json.dump({"timestamp": now}, scanned_fd, indent=2)
